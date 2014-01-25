bc8794943ca93b70f708239ba1b56f42d6a13936
grantmacken <grantmacken@gmail.com>
2014-01-26 09:13:01 +1300

    post to twit

--- .bin/new-post.py
+++ .bin/new-post.py
@@ -15,6 +15,26 @@ parser.add_argument('-i','--input', help='Input file name',required=True)
 parser.add_argument('-p','--post_type', help='Post Type',required=True)
 args = parser.parse_args()
 
+def base_n_encoder(alphabet):
+    """Return a encoder for a base-n encoded string
+   """
+    base = len(alphabet)
+    def f(num):
+        if (num == 0):
+            return alphabet[0]
+        parts = []
+        while num:
+            remainder = num % base
+            num //= base
+            parts.append(alphabet[remainder])
+        parts.reverse()
+        return ''.join(parts)
+    return f
+
+alpha = "0123456789ABCDEFGHJKLMNPQRSTUVWXYZ_abcdefghijkmnopqrstuvwxyz"
+#alpha = "23456789abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ-_.+="
+print len(alpha)
+encode = base_n_encoder(alpha)
 
 match=re.search(r'(article|note)', args.post_type)
 if match:
@@ -33,13 +53,25 @@ try:
     minute = time.strftime('%M',atime)
     second = time.strftime('%S',atime)
     sDate = year + '-' + month + '-' + day
-    #
+    shortYear = time.strftime('%y',atime)
+    dayOfYear = time.strftime('%y',atime)
+    shortTime =  hour + minute + second
+    iShortDateTime = int(shortYear + dayOfYear + shortTime)
+    nthPost = str(1).zfill(2)
+    print  nthPost
+
+    iShortDateNthPost = int(shortYear + dayOfYear +  nthPost)
+# if we are worried about duplication
+# nth post 00
+    print 'shortDateTime: ' +  str(iShortDateTime)
+    print 'datTimeEncoded: ' +  encode(iShortDateTime)
+    print 'iShortDateNthPost : ' +  encode(iShortDateNthPost)
     sTitle  = 'title: ' + args.input.replace('-', ' ')   + '\n'
     sPublished = 'published: ' + sDate + 'T' + hour + \
                 ':' + minute + ':' + second + '\n'
     sAuthor = 'author: ' +  config.get('project.author') + '\n'
     sID =  'id: tag:' +  config.get('project.domain') + ',' + sDate + ':' + \
-    postType +':' + args.input +'\n'
+    postType +':' + encode(iShortDateTime) +'\n'
 
     outFile = os.path.join( 'www' ,'_posts' , year + '-' + month + '-' + \
                            day + '-' +  args.input + '.md' )
@@ -47,29 +79,29 @@ try:
 except ValueError:
     raise
 
-print outFile
-
-try:
-    if not os.path.exists(outFile):
-        f = open(outFile, 'w')
-        f.write('---\n')
-        f.write(sTitle)
-        f.write(sAuthor)
-        f.write(sPublished)
-        f.write(sID)
-        f.write('summary: \n')
-        f.write('categories: \n')
-        f.write('---\n \n')
-        f.close()
-        cmd = "komodo " + outFile
-        os.system(cmd)
-        #os.utime(outFile, (now, now))
-
-        os.system("some_command with args")
-except OSError:
-    if os.path.exists(outFile):
-        # We are nearly safe
-        pass
-    else:
-        # There was an error on creation, so make sure we know about it
-        raise
+#print outFile
+#
+#try:
+#    if not os.path.exists(outFile):
+#        f = open(outFile, 'w')
+#        f.write('---\n')
+#        f.write(sTitle)
+#        f.write(sAuthor)
+#        f.write(sPublished)
+#        f.write(sID)
+#        f.write('summary: \n')
+#        f.write('categories: \n')
+#        f.write('---\n \n')
+#        f.close()
+#        cmd = "komodo " + outFile
+#        os.system(cmd)
+#        #os.utime(outFile, (now, now))
+#
+#        os.system("some_command with args")
+#except OSError:
+#    if os.path.exists(outFile):
+#        # We are nearly safe
+#        pass
+#    else:
+#        # There was an error on creation, so make sure we know about it
+#        raise
--- .bin/post-to-twitter.py
+++ .bin/post-to-twitter.py
@@ -1,37 +1,146 @@
 #!/usr/bin/env python
-import argparse
+
 import re
+import datetime
+import math
+
+import argparse
 import fileinput
+import markdown2
 
 from configobj import ConfigObj
 from twython import Twython
 
-config = ConfigObj('build.properties')
+def base_n_encoder(alphabet):
+    """Return a encoder for a base-n encoded string
+   """
+    base = len(alphabet)
+    def f(num):
+        if (num == 0):
+            return alphabet[0]
+        parts = []
+        while num:
+            remainder = num % base
+            num //= base
+            parts.append(alphabet[remainder])
+        parts.reverse()
+        return ''.join(parts)
+    return f
 
+alpha = "0123456789ABCDEFGHJKLMNPQRSTUVWXYZ_abcdefghijkmnopqrstuvwxyz"
+print len(alpha)
+encode = base_n_encoder(alpha)
+decode = base_n_decoder(alpha)
+
+#for v in (0, 4, 10, 60, 100, 481, 3881, 98839, 189238, 583832, 1039848):
+#    s = encode(v)
+#    d = decode(s)
+#    print v, s, d
+#    assert v==d, "Decoded value doesn't match"
+
+#
+config = ConfigObj('build.properties')
 
 parser = argparse.ArgumentParser(description='Ony one arg')
 parser.add_argument('-i','--input', help='Input file name',required=True)
 args = parser.parse_args()
 
 source_file_content = open(args.input, 'r').read()
-frontMatterSub = re.compile("-{3}[\s\S]+-{3}", re.M)
-status_content = frontMatterSub.sub('', source_file_content).strip()
+html = markdown2.markdown(source_file_content , extras=["metadata",
+"code-friendly", "cuddled-lists", "fenced-code-blocks", "header-ids" ,
+"smarty-pants"])
+metadata =  html.metadata
 
-print 'number of chars: ' + str(len(status_content))
+print metadata['id']
+#tag:markup.co.nz,2014-01-22:note:102623
+idNoteMatch = re.compile("^tag:.+:(note):.+$")
+idDateMatch = re.compile("^tag:.+,(\d{4}-\d{2}-\d{2}):(note):(.+)$")
+match = idDateMatch.match(metadata['id'])
+#match=re.search(r'"^tag:.+,(\d{4}-\d{2}-\d{2}):.+$"', metadata['id'])
+if match:
+    print 'match found'
+    dateString = match.group(1)
+    postTypeString = match.group(2)
+    identifierString = match.group(3)
+    print dateString
+    print postTypeString
+    print identifierString
+else:
+    print 'no dateString'
 
-APP_KEY = config.get('twitter.app.key')
-APP_SECRET = config.get('twitter.app.secret')
+l = dateString.split('-')
+l.append(str(identifierString))
+uHash = "".join(l)
+print int(uHash)
+shortLink = encode(int(uHash))
+d = decode(shortLink)
+print shortLink, d
 
-OAUTH_TOKEN = config.get('twitter.oauth.token')
-OAUTH_TOKEN_SECRET = config.get('twitter.oauth.token_secret')
+print 'url shortened to ' + shortLink
 
-twitter = Twython(APP_KEY, APP_SECRET,
-                  OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
-jsonResult = twitter.update_status(status=status_content)
-print jsonResult['id']
 
-for line in fileinput.input(args.input , inplace=1):
- print line,
- if line.startswith('categories:'):
-     print 'link-tweet-id: ' + str(jsonResult['id'])
+#frontMatterSub = re.compile("-{3}[\s\S]+-{3}", re.M)
+#status_content = frontMatterSub.sub('', source_file_content).strip()
+#
+## a list if #hashtags in status_content
+#lHashMatch=re.findall('#([\w]+)', status_content)
+#
+## a list if categories in front-matter
+#lCategories = metadata['categories'].split()
+#lnewCatergories = []
+#
+#for lCat in lCategories:
+#    if lCat in lHashMatch:
+#        print 'Category in text'
+#        print lCat
+#    else:
+#        print 'No category in text so will append  to text #' + lCat
+#        status_content += ' #'
+#        status_content += lCat
+#
+#print 'number of chars: ' + str(len(status_content))
+#
+#status_content +=  ' ('
+#status_content +=  config.get('project.domain')
+#status_content +=  ' '
+#status_content +=  shortLink
+#status_content +=  ')'
+#
+#print 'number of chars: ' + str(len(status_content))
+#print status_content
+##
+#if lHashMatch:
+#    for iHashMatch in lHashMatch:
+#        if iHashMatch in lCategories:
+#            print 'the HashTag in text IS in front matter categories'
+#        else:
+#            print 'the HashTag in text NOT front matter categories'
+#            metadata['categories'] += ' '
+#            metadata['categories'] += iHashMatch
+#
+#
+#print 'number of chars: ' + str(len(status_content))
+#print status_content
+#print metadata['categories']
+#
+#APP_KEY = config.get('twitter.app.key')
+#APP_SECRET = config.get('twitter.app.secret')
+#
+#OAUTH_TOKEN = config.get('twitter.oauth.token')
+#OAUTH_TOKEN_SECRET = config.get('twitter.oauth.token_secret')
+#
+#twitter = Twython(APP_KEY, APP_SECRET,
+#                  OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+#
+#jsonResult = twitter.update_status(status=status_content)
+#
+#for line in fileinput.input(args.input , inplace=1):
+#    if line.startswith('categories:'):
+#        print 'categories: ' + metadata['categories']
+#        if metadata['link-tweet-id'] is None:
+#            print 'link-tweet-id: ' + str(jsonResult['id']),
+#        if metadata['link-shortened'] is None:
+#            print 'link-shortened: ' + shortLink
+#    else:
+#        print line,
