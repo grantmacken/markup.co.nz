de9357cf5226e1a80ede7673151f5a82936cbc87
grantmacken <grantmacken@gmail.com>
2014-02-19 19:57:56 +1300

    bump

--- config.rb
+++ config.rb
@@ -7,8 +7,8 @@ require 'omg-text'
 http_path = "/"
 css_dir = ".deploy/resources/styles"
 sass_dir = "www/resources/styles"
-images_dir = ".deploy/resources/images"
-javascripts_dir = ".deploy/resources/scripts"
+images_dir = ".deploy/local/resources/images"
+javascripts_dir = ".deploy/local/resources/scripts"
 # You can select your preferred output style here (can be overridden via the command line):
 output_style = :compact #:compressed #:expanded or :nested or :compact or :compressed
 # To disable debugging comments that display the original location of your selectors. Uncomment:
